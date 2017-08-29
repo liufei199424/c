8797ba77c58baf82a6078e58788527e2697c0d5d
fhw <877172774@qq.com>
2017-08-29 10:41:11 +0800

    fhw

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main () {
-    printf("Hello World!");
+	int i = 0;
+	int j = 0;
+	
+	for(i = 0; i < 10; i++) {
+		for(j = 1; j <= i; j++) {
+			printf("%dx%d=%2d ", i, j, i * j);
+		}
+		printf("\n");
+	}
 
     return 0;
 }
